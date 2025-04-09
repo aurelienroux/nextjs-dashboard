9dba8f366ba959fc845477c840978f0568f15f50
Aurélien Roux <aureroux13@gmail.com>
2025-04-09 20:32:35 +0200

    populate card data

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -14,11 +14,8 @@ export default async function RevenueChart({
 }: {
   revenue: Revenue[];
 }) {
-  console.log(revenue);
-
   const chartHeight = 350;
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
-  console.log(yAxisLabels, topLabel);
 
   if (!revenue || revenue.length === 0) {
     return <p className="mt-4 text-gray-400">No data available.</p>;
